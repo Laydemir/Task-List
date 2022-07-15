947f922c6b067bdd1e6648922c141da5a3b70b88
Laydemir <voldvakoff@gmail.com>
2022-07-15 17:43:21 +0300

    function add task

--- js/main.js
+++ js/main.js
@@ -1,11 +1,50 @@
 // Находим элементы на странице
-
-
-
-/*const form = document.querySelector('#form');
+const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
 const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
+
+form.addEventListener('submit', addTask);
+
+function addTask(event) {
+	// Отменяем отправку формы
+	event.preventDefault();
+
+	// Достаем текст задачи из поля ввода
+	const taskText = taskInput.value;
+
+	//формируем разметку для новой задачи
+	const taskHTML = `
+		<li class="list-group-item d-flex justify-content-between task-item">
+			<span class="task-title">${taskText}</span>
+			<div class="task-item__buttons">
+				<button type="button" data-action="done" class="btn-action">
+					<img src="./img/tick.svg" alt="Done" width="18" height="18">
+				</button>
+				<button type="button" data-action="delete" class="btn-action">
+					<img src="./img/cross.svg" alt="Done" width="18" height="18">
+				</button>
+			</div>
+		</li>`;
+
+	//добавить задачу на чтраницу
+	tasksList.insertAdjacentHTML('beforeend', taskHTML);
+
+	//очищвем поле ввода и возвращаем фокус на него
+	taskInput.value = ""
+	taskInput.focus()
+
+	//скрываем запись "Список дел пуст" (добавляем класс NONE) если появляются задачи
+	if (tasksList.children.length > 1) {
+		emptyList.classList.add('none')
+	}
+}
+
+
+
+
+/*
+
 
 let tasks = [];
 
@@ -16,7 +55,7 @@ if (localStorage.getItem('tasks')) {
 
 checkEmptyList();
 
-form.addEventListener('submit', addTask);
+
 tasksList.addEventListener('click', deleteTask);
 tasksList.addEventListener('click', doneTask);
 
@@ -26,7 +65,6 @@ function addTask(event) {
 	event.preventDefault();
 
 	// Достаем текст задачи из поля ввода
-	const taskText = taskInput.value;
 
 	// Описываем задачу в виде объекта
 	const newTask = {
